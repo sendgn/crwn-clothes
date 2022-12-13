7e47cbb8b5041a6918c0b08bf7933ea2984fa51b
Aleksandr Polozhenskii <polozhenskii@gmail.com>
2022-12-13 10:04:15 +0300

    Add styles for navigation

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,5 +1,6 @@
 import { Outlet, Link } from 'react-router-dom';
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
+import './navigation.styles.scss';
 
 const Navigation = () => {
     return (
