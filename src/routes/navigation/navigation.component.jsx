93ee052fbf7f5445c36ccfbf099d2e26db1ba56a
Aleksandr Polozhenskii <polozhenskii@gmail.com>
2022-12-22 23:21:28 +0300

    Add cart icon component as well as cart dropdown and style them

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,11 +1,15 @@
 import { Outlet, Link } from 'react-router-dom';
 import { useContext } from 'react';
 
-import './navigation.styles.scss';
+import { UserContext } from '../../contexts/user.context';
+
+import CartIcon from '../../components/cart-icon/cart-icon.component';
+import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
 
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
-import { UserContext } from '../../contexts/user.context';
 import { signOutUser } from '../../utils/firebase/firebase.utils';
+
+import './navigation.styles.scss';
 
 const Navigation = () => {
     const { currentUser } = useContext(UserContext);
@@ -32,7 +36,9 @@ const Navigation = () => {
                             SIGN IN
                         </Link>
                     )}
+                    <CartIcon />
                 </div>
+                <CartDropdown />
             </div>
             <Outlet />
         </>
