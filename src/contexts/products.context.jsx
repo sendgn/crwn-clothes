e16c7fa4271a46ff05c176eb5e24b7af410c86b7
Aleksandr Polozhenskii <polozhenskii@gmail.com>
2023-01-04 22:12:29 +0300

    Create getting category map from Firebase function

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -1,8 +1,10 @@
 import { createContext, useEffect, useState } from 'react';
 
-import { addCollectionAndDocuments } from '../utils/firebase/firebase.utils';
-
+/* Step 1. Uncomment imports before doing Step 2. */
+// import { addCollectionAndDocuments } from '../utils/firebase/firebase.utils';
 // import SHOP_DATA from '../shop-data.js';
+
+import { getCategoriesAndDocuments } from '../utils/firebase/firebase.utils';
 
 export const ProductsContext = createContext({
     products: [],
@@ -11,10 +13,22 @@ export const ProductsContext = createContext({
 export const ProductsProvider = ({ children }) => {
     const [products, setProducts] = useState([]);
 
-    // Uncomment to create collection in Firebase from an object array
+    /* Step 2. Uncomment to create categories collection in Firebase off of SHOP_DATA.
+     * Step 3. Comment it again after the first render
+     *         not to rewrite firebase collection over and over again on each page update,
+     *         it should be done only once.
+     */
     // useEffect(() => {
     //     addCollectionAndDocuments('categories', SHOP_DATA);
     // }, []);
+
+    useEffect(() => {
+        const getCategoriesMap = async () => {
+            const categoryMap = await getCategoriesAndDocuments();
+        }
+
+        getCategoriesMap();
+    }, []);
 
     const value = { products };
 
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -18,6 +18,8 @@ import {
     setDoc,
     collection,
     writeBatch,
+    query,
+    getDocs
 } from 'firebase/firestore';
 
 const firebaseConfig = {
@@ -50,6 +52,20 @@ export const addCollectionAndDocuments = async (
     });
 
     await batch.commit();
+}
+
+export const getCategoriesAndDocuments = async () => {
+    const collectionRef = collection(db, 'categories');
+    const q = query(collectionRef);
+
+    const querySnapshot = await getDocs(q);
+    const categoryMap = querySnapshot.docs.reduce((acc, docSnapshot) => {
+        const { title, items } = docSnapshot.data();
+        acc[title.toLowerCase()] = items;
+        return acc;
+    }, {});
+    
+    return categoryMap;
 }
 
 // Instantiate and set up Google provider
