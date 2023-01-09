b0baf4088e5a1fd4d3d70b5b8422692ecd9ad946
Aleksandr Polozhenskii <polozhenskii@gmail.com>
2023-01-09 23:02:04 +0300

    Convert directory item styles into styled component

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,23 +1,20 @@
-import { Link } from 'react-router-dom';
-
-import './directory-item.styles.scss';
+import {
+    DirectoryItemContainer,
+    BackgroundImage,
+    Body,
+} from './directory-item.styles';
 
 const DirectoryItem = ({ category }) => {
     const { title, imageUrl } = category;
 
     return (
-        <Link className='directory-item-container' to={`/shop/${title}`}>
-            <div
-                className='background-image'
-                style={{
-                    backgroundImage: `url(${imageUrl})`,
-                }}
-            />
-            <div className='body'>
-                <h2>{title.toUpperCase()}</h2>
+        <DirectoryItemContainer>
+            <BackgroundImage imageUrl={imageUrl} />
+            <Body>
+                <h2>{title}</h2>
                 <p>Shop Now</p>
-            </div>
-        </Link>
+            </Body>
+        </DirectoryItemContainer>
     );
 }
 
