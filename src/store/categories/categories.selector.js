11c1f82df9e9db36a12474271e82cabc44ad8737
Aleksandr Polozhenskii <polozhenskii@gmail.com>
2023-01-11 01:37:52 +0300

    Memoize categories reducer slice

--- src/store/categories/categories.selector.js
+++ src/store/categories/categories.selector.js
@@ -1,10 +1,22 @@
-export const selectCategoriesMap = (state) => {
-    console.log('selector fired');
+import { createSelector } from 'reselect';
 
-    return state.categories.categories
-        .reduce((acc, category) => {
+// Get only categories slice of a global app state
+const selectCategoriesReducer = (state) => state.categories;
+
+// Memoized selector for the whole categories slice
+export const selectCategories = createSelector(
+    [selectCategoriesReducer],
+    (categoriesSlice) => categoriesSlice.categories
+);
+
+// Memoized selector for the categories map
+// That means - don't do reduce() if categories array is still the same
+export const selectCategoriesMap = createSelector(
+    [selectCategories],
+    (categories) =>
+        categories.reduce((acc, category) => {
             const { title, items } = category;
             acc[title.toLowerCase()] = items;
             return acc;
-        }, {});
-}
+        }, {})
+);
