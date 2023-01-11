fb3cc1c2cca8354c0fadea9092fabd7cad16e60d
Aleksandr Polozhenskii <polozhenskii@gmail.com>
2023-01-11 19:16:11 +0300

    Add redux-thunk to fetch categories array

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -2,8 +2,7 @@ import { useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 
-import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils';
-import { setCategories } from '../../store/categories/categories.action';
+import { fetchCategoriesAsync } from '../../store/categories/categories.action';
 
 import CategoriesPreview from '../categories-preview/categories-preview.component';
 import Category from '../category/category.component';
@@ -25,11 +24,7 @@ const Shop = () => {
     const dispatch = useDispatch();
 
     useEffect(() => {
-        const getCategoriesMap = async () => {
-            const categoriesArray = await getCategoriesAndDocuments();
-            dispatch(setCategories(categoriesArray));
-        }
-        getCategoriesMap();
+        dispatch(fetchCategoriesAsync());
     }, []);
 
     return (
